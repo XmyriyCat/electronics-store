835597c763e366edf5a3ec2b458a3ef40b642867
Wazowskiy <SilverInTrash@protonmail.com>
2023-04-20 14:43:41 +0200

    Unused usings was removed.

--- Data/Repositories/Implementations/ManufacturerRepository.cs
+++ Data/Repositories/Implementations/ManufacturerRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Data.Repositories.Contracts;
 using DomainModels;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Repositories.Implementations
 {
